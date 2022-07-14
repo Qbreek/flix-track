a349113363c6cd876982f2aede8e5803c4016efc
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-07-14 14:17:14 +0300

    build : add TMDB API key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'eb28ab69f00b1174a34d2077d24ec4dd',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'eb28ab69f00b1174a34d2077d24ec4dd',
 };
 
 /*
